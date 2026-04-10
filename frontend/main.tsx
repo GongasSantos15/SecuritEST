a9c9661429637b2cb2d13467cdb413ff903a8d8d
GongasSantos15 <goncalo.c.r.santos@protonmail.com>
2026-04-10 17:42:24 +0100

    Change main to main.tsx and the index.html file

--- frontend/main.tsx
+++ frontend/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './App' // Confirma se o teu App.tsx exporta como 'App'
-import './GlobalStyle' // Se tiveres estilos globais
+import App from './App' // Tenta sem as chavetas e sem o .tsx
+import './GlobalStyle' 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
