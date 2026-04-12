92530b2cbc0446829736b507faf41bd8308203e0
GongasSantos15 <goncalo.c.r.santos@protonmail.com>
2026-04-12 17:41:49 +0100

    Changes to make the suffix fixed

--- infra/main.tf
+++ infra/main.tf
@@ -6,28 +6,16 @@ resource "azurerm_resource_group" "rg" {
 
 # Cria o Storage Account para guardar os logs de análise e para o funcionamento da Azure Function
 resource "azurerm_storage_account" "storage" {
-  name                     = "stsecuritest${random_string.suffix.result}"
+  name                     = "stsecuritest${var.suffix}"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-# Gerador de sufixo aleatório para garantir nomes únicos no Azure
-# O keepers garante que o sufixo nunca muda enquanto o resource group for o mesmo
-resource "random_string" "suffix" {
-  length  = 6
-  special = false
-  upper   = false
-
-  keepers = {
-    resource_group = var.securitest
-  }
-}
-
 # Cria uma base de dados NoSQL (Azure CosmosDB) para guardar os resultados dos scans
 resource "azurerm_cosmosdb_account" "db" {
-  name                = "cosmos-securitest-${random_string.suffix.result}"
+  name                = "cosmos-securitest-${var.suffix}"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   offer_type          = "Standard"
@@ -52,7 +40,7 @@ resource "azurerm_service_plan" "plan" {
 
 # A Azure Function que vai correr o Docker
 resource "azurerm_linux_function_app" "scanner_func" {
-  name                       = "func-scanner-${random_string.suffix.result}"
+  name                       = "func-scanner-${var.suffix}"
   resource_group_name        = azurerm_resource_group.rg.name
   location                   = azurerm_resource_group.rg.location
   storage_account_name       = azurerm_storage_account.storage.name
@@ -71,7 +59,7 @@ resource "azurerm_linux_function_app" "scanner_func" {
 
 # Recurso para alojar o Frontend
 resource "azurerm_linux_web_app" "frontend" {
-  name                = "app-securitest-frontend-${random_string.suffix.result}"
+  name                = "app-securitest-frontend-${var.suffix}"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   service_plan_id     = azurerm_service_plan.plan.id
@@ -80,16 +68,16 @@ resource "azurerm_linux_web_app" "frontend" {
     application_stack {
       node_version = "18-lts"
     }
-    always_on = false
+    always_on        = false
     app_command_line = "npx serve -s /home/site/wwwroot -l 8080"
   }
 
   app_settings = {
-  "WEBSITES_PORT"                = "8080"
-  "WEBSITE_NODE_DEFAULT_VERSION" = "~18"
-  "SCM_DO_BUILD_DURING_DEPLOYMENT" = "false"
-  "WEBSITE_RUN_FROM_PACKAGE"       = "0"
-}
+    "WEBSITES_PORT"                  = "8080"
+    "WEBSITE_NODE_DEFAULT_VERSION"   = "~18"
+    "SCM_DO_BUILD_DURING_DEPLOYMENT" = "false"
+    "WEBSITE_RUN_FROM_PACKAGE"       = "0"
+  }
 }
 
 # Output para saberes o URL do teu site no fim
--- infra/variables.tf
+++ infra/variables.tf
@@ -4,6 +4,12 @@ variable "securitest" {
   default     = "rg-securitest"
 }
 
+variable "suffix" {
+  description = "Sufixo fixo para garantir nomes únicos"
+  type        = string
+  default     = "1xyiu5"  # ← o sufixo atual, fica sempre este
+}
+
 variable "switzerlandNorth" {
   description = "Região do Azure"
   type        = string
